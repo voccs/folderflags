d9815fb0be08efdf53b2b3a399364e558e77df93
voccs <ryan@voccs.com>
2011-07-03 22:05:52 -0700

    moving folderflags out of its own menu / context menu option and into a folder properties tab

--- chrome/content/folderflags.js
+++ chrome/content/folderflags.js
@@ -1,97 +1,76 @@
-var FolderFlags = {};
+var FolderFlags = {
+    // flags; watch chrome://messenger/locale/messenger.properties for labels
+    'flagList': {
+        'trash' : 0x0100,
+        'sent' : 0x0200,
+        'drafts' : 0x0400,
+        'outbox' : 0x0800,
+        'inbox' : 0x1000,
+        'archives' : 0x4000,
+        'templates' : 0x400000,
+        'junk' : 0x40000000
+    }
+};
 
-// flags; watch chrome://messenger/locale/messenger.properties for labels
-var gFlagList = { 'trash' : 0x0100,
-                  'sent' : 0x0200,
-                  'drafts' : 0x0400,
-                  'outbox' : 0x0800,
-                  'inbox' : 0x1000,
-                  'archives' : 0x4000,
-                  'templates' : 0x400000,
-                  'junk' : 0x40000000 };
+FolderFlags._checkEnv = function() {
+    // gMsgFolder should exist or all is forfeit
+    if (typeof gMsgFolder === "undefined") {
+        dump("gMsgFolder doesn't exist, this is an insurmountable problem!");
+        return false;
+    } else {
+        return true;
+    }
+};
 
 FolderFlags.onLoad = function() {
-    var preselectedFolderURI;
-    var folderResource;
-    var serverFolderType;
-    var folderName;
-    var folder;
-    var flagsBundle = document.getElementById("bundle_folderflags");
+    if (!FolderFlags._checkEnv())
+        return;
+
     var msgrBundle = document.getElementById("bundle_messenger");
-
-    var RDF = Components.classes["@mozilla.org/rdf/rdf-service;1"].getService(Components.interfaces.nsIRDFService);
-
-    if (window.arguments[0].preselectedURI) {
-        try {
-            preselectedFolderURI = window.arguments[0].preselectedURI;
-        } catch (ex) { }
-    } else {
-        dump("no preselected URI\n");
-    }
-
-    if (window.arguments[0].name) {
-        folderName = window.arguments[0].name;
-    }
-
-    if (window.arguments[0].serverType) {
-        serverFolderType = window.arguments[0].serverType;
-    }
-
-    folderResource = RDF.GetResource(preselectedFolderURI);
-
-    if (folderResource)
-        folder = folderResource.QueryInterface(Components.interfaces.nsIMsgFolder);
-
     var flags = document.getElementById("folderflags-flaglist");
 
-    // set folder name
-    var folderNameLabel = document.getElementById("folderName");
-    folderNameLabel.value = folderName;
+    var folderNameLabel = document.getElementById("folderflags-folderName");
+    folderNameLabel.value = window.arguments[0].name
 
-    for (var flag in gFlagList) {
+    // Fill out a grid of viable flags
+    for (var flag in FolderFlags.flagList) {
         var checkbox = document.createElement("checkbox");
         var labelKey = flag + "FolderName";
         var label = msgrBundle.getString(labelKey);
         checkbox.setAttribute("class", "indent");
         checkbox.setAttribute("id", "checkbox_" + flag);
         checkbox.setAttribute("label", label);
-        if (folder.flags & gFlagList[flag])
+        if (gMsgFolder.flags & FolderFlags.flagList[flag])
             checkbox.setAttribute("checked", "true");
         flags.appendChild(checkbox);
     }
-}
+};
+
+FolderFlags.onUnload = function() {
+    window.removeEventListener("load", FolderFlags.onLoad, false);
+    window.removeEventListener("unload", FolderFlags.onUnload, false);
+    window.removeEventListener("dialogaccept", FolderFlags.save, false);
+};
 
 FolderFlags.save = function() {
-    var preselectedFolderURI;
-    var folderResource;
-    var folder;
-    var RDF = Components.classes["@mozilla.org/rdf/rdf-service;1"].getService(Components.interfaces.nsIRDFService);
+    if (!FolderFlags._checkEnv())
+        return;
 
-    if (window.arguments[0].preselectedURI) {
-        try {
-            preselectedFolderURI = window.arguments[0].preselectedURI;
-        } catch (ex) { }
-    } else {
-        dump("no preselected URI\n");
-    }
-
-    folderResource = RDF.GetResource(preselectedFolderURI);
-
-    if (folderResource)
-        folder = folderResource.QueryInterface(Components.interfaces.nsIMsgFolder);
-
-    for (var flag in gFlagList) {
+    for (var flag in FolderFlags.flagList) {
         var check = document.getElementById("checkbox_" + flag);
 
         if (check.checked) {
             // store setting and set flag
-            folder.setFlag(gFlagList[flag]);
+            gMsgFolder.setFlag(FolderFlags.flagList[flag]);
         } else {
             // remove setting and clear flag
-            folder.clearFlag(gFlagList[flag]);
+            gMsgFolder.clearFlag(FolderFlags.flagList[flag]);
         }
     }
 
-    window.close();
     window.opener.document.getElementById('folderTree').builder.rebuild();
-}
+};
+
+window.addEventListener("load", FolderFlags.onLoad, false);
+window.addEventListener("unload", FolderFlags.onUnload, false);
+window.addEventListener("dialogaccept", FolderFlags.save, false);
